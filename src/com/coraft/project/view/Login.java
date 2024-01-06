7b72973786a9d70502f13edf2a7fe6442e4af878
KIMHYEONJI13 <https://github.com/KIMHYEONJI13>
2024-01-06 16:18:25 +0900

    로그인 페이지 생성

---
+++ src/com/coraft/project/view/Login.java
@@ -0,0 +1,37 @@
+package com.coraft.project.view;
+
+import java.util.Scanner;
+
+public class Login {
+
+    Scanner sc = new Scanner(System.in);
+
+
+    public void mainLogin() {
+
+        System.out.println("안녕하세요~ coraft 입니다.");
+        System.out.println("클래스 신청을 위해 로그인 페이지로 이동합니다.");
+        System.out.println("========================================");
+        System.out.println("1. 로그인");
+        System.out.println("2. 회원가입");
+        System.out.println("9. 종료");
+        System.out.println("========================================");
+
+        System.out.print("메뉴를 선택해주세요 : ");
+        int num = sc.nextInt();
+
+        switch (num) {
+            case 1 : ; break;
+            case 2 : ; break;
+            case 9 : ; break;
+            default: System.out.println("잘못된 번호를 입력하셨습니다."); break;
+
+        }
+
+
+
+    }
+
+
+
+}
