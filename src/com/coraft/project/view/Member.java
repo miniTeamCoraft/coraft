84982b509eafefa90abb8322be5b420c6d05ef11
JiYun9796 <pisaek102@gmail.com>
2024-01-08 17:56:51 +0900

    멤버 수정

--- src/com/coraft/project/view/Member.java
+++ src/com/coraft/project/view/Member.java
@@ -6,19 +6,19 @@ public class Member {
      * 5명 인적사항 만들기로 함.*/
 
     public static void main(String[] args) {
-        Applylogin member1 = new Applylogin("아이디1", "비번1", "이름1");
+        Applylogin member1 = new Applylogin("user1", "1234", "김아름");
         System.out.println(member1.gim());
 
-        Applylogin member2 = new Applylogin("아이디2", "비번2", "이름2");
+        Applylogin member2 = new Applylogin("user2", "5678", "유한별");
         System.out.println(member2.gim());
 
-        Applylogin member3 = new Applylogin("아이디3", "비번3", "이름3");
+        Applylogin member3 = new Applylogin("user3", "9101112", "안지후");
         System.out.println(member3.gim());
 
-        Applylogin member4 = new Applylogin("아이디4", "비번4", "이름4");
+        Applylogin member4 = new Applylogin("user4", "13141516", "강우주");
         System.out.println(member4.gim());
 
-        Applylogin member5 = new Applylogin("아이디5", "비번5", "이름5");
+        Applylogin member5 = new Applylogin("user5", "17181920", "조하나");
         System.out.println(member5.gim());
     }
 
