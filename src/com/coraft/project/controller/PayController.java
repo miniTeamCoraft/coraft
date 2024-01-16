8629d4da73cc259cf830fe7ffa87c15e4918e882
KIMHYEONJI13 <https://github.com/KIMHYEONJI13>
2024-01-16 16:42:40 +0900

    결제관련 보완 업데이트

--- src/com/coraft/project/controller/PayController.java
+++ src/com/coraft/project/controller/PayController.java
@@ -17,7 +17,7 @@ public class PayController {
     // 포인트 차감 결제
     public ArrayList<LectureDTO> payBonusMember(MemberDTO user, LectureDTO lecture) {
 
-        int getPoint = (int)(lecture.getLecPrice() * 0.05);
+        /*int getPoint = (int)(lecture.getLecPrice() * 0.05);
 
         System.out.println("-------------------------------------------------");
         System.out.println("CORAFT 규정에 따라 결제금액의 5%인 " + getPoint + "포인트 적립됩니다.");
@@ -28,7 +28,7 @@ public class PayController {
 
         int newPrice = lecture.getLecPrice() - user.getPoint();
 
-        if(newPrice < 0) {
+        if(newPrice < 0 ) {
             System.out.println("보유하신 포인트가 결제할 금액보다 많습니다.");
             System.out.println("-------------------------------------------------");
             System.out.print("사용하실 포인트를 입력해 주세요 : ");
@@ -48,20 +48,17 @@ public class PayController {
             int usingPoint = lecture.getLecPrice() - user.getPoint();
             System.out.println("포인트를 차감한 " + usingPoint + "원은 자동 카드결제됩니다.");
             user.setPoint(getPoint);
-            System.out.println("-------------------------------------------------");
-            System.out.println("카드결제가 진행됩니다.");
-            System.out.println(". . .");
-            System.out.println("카드 결제가 완료되었습니다. ");
+            pay();
 
         } else if (newPrice == 0) {
             System.out.println(user.getPoint() + "포인트 차감되었습니다.");
             user.setPoint(getPoint);
             System.out.println(". . .");
             System.out.println("포인트로 결제 완료되었습니다.");
-        }
+        }*/
+        usePayPoint(user, lecture);
 
         lec.add(new LectureDTO(lecture.getLecName(), lecture.getDate(), lecture.getTime(), lecture.getLecPrice()));
-
         menu.mainMenu(user);
 
         return lec;
@@ -71,23 +68,64 @@ public class PayController {
     // 카드결제
     public ArrayList<LectureDTO> payCardMember(MemberDTO user, LectureDTO lecture) {
 
-        int getPoint = (int)(lecture.getLecPrice() * 0.05);
+        useCardPay(user, lecture);
+        pay();
 
-        System.out.println(user.getName() + "님의 결제금액은 " + lecture.getLecPrice() + "원 입니다.");
-        System.out.println("CORAFT 규정에 따라 결제금액의 5%인 " + getPoint + "포인트 적립됩니다.");
-        user.setPoint(getPoint + user.getPoint());
+        lec.add(new LectureDTO(lecture.getLecName(), lecture.getDate(), lecture.getTime(), lecture.getLecPrice()));
+        menu.mainMenu(user);
+
+        return lec;
+    }
+
+    public void usePayPoint(MemberDTO user, LectureDTO lecture) {
+
+        System.out.println("-------------------------------------------------");
+        System.out.println("CORAFT 규정에 따라 포인트차감 금액을 제외한 \n결제금액의 5%가 포인트로 적립됩니다.");
         System.out.println("적립된 포인트는 다음 수강신청 시 사용하실 수 있습니다.");
+        System.out.println("-------------------------------------------------");
+        System.out.println(user.getName() + "님의 결제금액은 " + lecture.getLecPrice() + "원 입니다.");
+        System.out.println("보유하신 포인트는 " + user.getPoint() + "포인트 입니다.");
+        System.out.println("-------------------------------------------------");
 
+
+        System.out.print("사용하실 포인트를 입력해 주세요 : ");
+        int usePoint = sc.nextInt();
+
+        int payNewPrice = lecture.getLecPrice() - usePoint;
+        if (payNewPrice > 0) {
+            int minusPoint = user.getPoint() - usePoint;
+            System.out.println(usePoint + "포인트 사용하여 " + minusPoint + "포인트 남았습니다.");
+            System.out.println("-------------------------------------------------");
+            System.out.println(usePoint + "포인트를 차감한 " + payNewPrice + "원은 자동 카드결제됩니다.");
+            int getPoint = (int) (payNewPrice * 0.05);
+            user.setPoint(minusPoint + getPoint);
+            pay();
+
+        } else if (payNewPrice < 0) {
+            System.out.println("포인트 사용 금액이 결제할 금액보다 많습니다. \n포인트를 다시 입력해 주세요"); ;
+            usePayPoint(user, lecture);
+        } else {
+            System.out.println(usePoint + "포인트를 사용하여 전액 포인트 결제 되었습니다.");
+
+        }
+
+    }
+
+    public void useCardPay(MemberDTO user, LectureDTO lecture) {
+        System.out.println(user.getName() + "님의 결제금액은 " + lecture.getLecPrice() + "원 입니다.");
+
+        int getPoint = (int)(lecture.getLecPrice() * 0.05);
+        System.out.println("CORAFT 규정에 따라 결제금액의 5%인 " + getPoint + "포인트 적립됩니다.");
+        System.out.println("적립된 포인트는 다음 수강신청 시 사용하실 수 있습니다.");
+        user.setPoint(getPoint + user.getPoint());
+
+    }
+
+    public void pay() {
         System.out.println("-------------------------------------------------");
         System.out.println("카드결제가 진행됩니다.");
         System.out.println(". . .");
         System.out.println("카드 결제가 완료되었습니다. ");
-
-        lec.add(new LectureDTO(lecture.getLecName(), lecture.getDate(), lecture.getTime(), lecture.getLecPrice()));
-
-        menu.mainMenu(user);
-
-        return lec;
     }
 
     public void userSelectLec() {
@@ -102,3 +140,4 @@ public class PayController {
     }
 
 }
+
